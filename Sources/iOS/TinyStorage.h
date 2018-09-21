bfe9b2c69b9f299d0b084279466b1cc238cd3611
Roy Hsu <roy.hsu@tinyworld.cc>
2018-09-21 20:25:12 +0800

    add Xcode project.

---
+++ Sources/iOS/TinyStorage.h
@@ -0,0 +1,18 @@
+//
+//  TinyStorage.h
+//  TinyStorage
+//
+//  Created by Roy Hsu on 2018/9/21.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinyStorage.
+FOUNDATION_EXPORT double TinyStorageVersionNumber;
+
+//! Project version string for TinyStorage.
+FOUNDATION_EXPORT const unsigned char TinyStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyStorage/PublicHeader.h>
+
+
